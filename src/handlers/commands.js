2aff179d821b000d10ef8954f9f11d4dd0a128cd
Renato Freitas <renato_freitas13@icloud.com>
2022-09-24 14:28:54 -0300

    melhorando saida no terminal

--- src/handlers/commands.js
+++ src/handlers/commands.js
@@ -1,24 +1,26 @@
 const { reddirSync, readdirSync } = require("fs");
 
+let count = 0;
 module.exports = (client) => {
-  slashCommands = [];
-  const load = async (dirs) => {
-    const commands = readdirSync(`./commands/${dirs}/`).filter((f) =>
-      f.endsWith(".js")
-    );
-    for (let file of commands) {
-      const command = require(`../commands/${dirs}/${file}`);
-      client.commands.set(command.name, command);
-      slashCommands.push(command);
+	slashCommands = [];
+	const load = async (dirs) => {
+		const commands = readdirSync(`./commands/${dirs}/`).filter((f) =>
+			f.endsWith(".js")
+		);
+		for (let file of commands) {
+			const command = require(`../commands/${dirs}/${file}`);
+			client.commands.set(command.name, command);
+			slashCommands.push(command);
 
-      console.log(`O comando ${command.name} foi carregado com sucesso!`);
-    }
-  };
-  client.on("ready", async () => {
-    await client.application.commands.set(slashCommands);
-    console.log("Todos os comandos carregados com sucesso!");
-  });
-  readdirSync("./commands/").forEach((dirs) => {
-    load(dirs);
-  });
+			console.log(`O comando ${command.name} foi carregado com sucesso!`);
+			count++;
+		}
+	};
+	client.on("ready", async () => {
+		await client.application.commands.set(slashCommands);
+		console.log(`Todos os ${count} comandos carregados!`);
+	});
+	readdirSync("./commands/").forEach((dirs) => {
+		load(dirs);
+	});
 };
