f6126ae00b4c0459003e69405e5b4fa0db242a9f
Renato Freitas <renato_freitas13@icloud.com>
2022-09-24 11:58:36 -0300

    comando para o bot conectar na call #29

---
+++ src/commands/audio/join.js
@@ -0,0 +1,41 @@
+const {
+	VoiceConnectionStatus,
+	joinVoiceChannel,
+	entersState,
+} = require("@discordjs/voice");
+
+async function connectToChannel(interaction) {
+	const connection = joinVoiceChannel({
+		channelId: interaction.member.voice.channel.id,
+		guildId: interaction.guild.id,
+		adapterCreator: interaction.guild.voiceAdapterCreator,
+	});
+	try {
+		await entersState(connection, VoiceConnectionStatus.Ready, 2_000);
+		return connection;
+	} catch (error) {
+		connection.destroy();
+		throw error;
+	}
+}
+
+module.exports = {
+	name: "join",
+	description: "entra no seu canal de voz",
+	type: 1,
+
+	run: async (client, interaction, args) => {
+		if (!interaction.member.voice.channel)
+			return interaction.reply(
+				"Você precisa estar em um canal de voz para usar este comando!"
+			);
+		else {
+			try {
+				connectToChannel(interaction);
+				interaction.reply("entrou no canal de voz");
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	},
+};
