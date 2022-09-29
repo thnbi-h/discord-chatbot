a505948e92a8320e3aba2928d370ba4a0b95b3f0
Renato Freitas <renato_freitas13@icloud.com>
2022-09-29 01:41:49 -0300

    :file_folder: organizando repositorio #34

--- events/interactions/interactionCreate.js
+++ events/interactions/interactionCreate.js
@@ -1,7 +1,7 @@
 module.exports = async (client, interaction) => {
    if(!interaction.type === 2) return;
    const command = client.commands.get(interaction.commandName);
-   const args = interaction.options
+   const args = interaction.options  
    if(!command) return;
    try {
       await command.run(client, interaction, args);
